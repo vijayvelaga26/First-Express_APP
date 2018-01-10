43801c8f937331ee1b0968f828a3e3f1d94614cf
Vijay Velaga <vijay.velaga@gmail.com>
2018-01-10 17:51:26 -0500

    Added new routes

--- app.js
+++ app.js
@@ -10,7 +10,25 @@ app.get("/new",function(req,res){
     res.send("Just finished creating the first express app !!!! ")
 })
 
+app.get("/new/:name",function(req,res){
+    var name= req.params.name;
+    res.send("Hello "+ name +" ,How are you doing Today?");
+})
+
+app.get ("/new/:name/:number",function(req,res){
+    var name= req.params.name;
+    var number= req.params.number; var print="";
+    for(var i=0;i<number;i++){
+       print=print +name+ " "; 
+    }
+    res.send(print)
+})
+
+app.get("*",function(req,res){
+    res.send("Currently not available , but will be Coming Soon !!!!");
+})
+
 app.listen(3000,function(){
-    console.log("Server is running on port 3000. Check /new url too");
+    console.log("Server is running on port 3000. Check /new & /new/anyName & /new/anyName/anyNumber");
 })
 
